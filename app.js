1c01c2efbbc72bf838e35436352a6b9b284e2a77
Yulonda Joy Wyatt <yulondawyatt@gmail.com>
2025-06-27 09:14:47 -0400

    Successful in completing getEmployeebyId

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import client from './db/client.js';
 
 
 import { getEmployees } from './db/queries/employees.js';
+import { getEmployee } from './db/queries/employees.js';
+
 
 app.get('/', (req, res, next) => {
   res.send('Welcome to the Fullstack Employees API');
@@ -15,4 +17,23 @@ app.get('/', (req, res, next) => {
 app.get('/employees', async (req, res, next) => {
   const allEmployees = await getEmployees();
   res.send(allEmployees);
+});
+
+
+
+app.get('/employees/:id', async (req, res, next) =>{
+  const id = Number(req.params.id);
+
+  try {
+    const employeeById = await getEmployee(id);
+
+    if(!employeeById) {
+      return res.status(404).send({ error: `Employee by that ID does not exist.` });
+    }
+      res.send(employeeById);
+
+  } catch (error) {
+    next(error);
+  }
+  
 });
--- db/queries/employees.js
+++ db/queries/employees.js
@@ -5,34 +5,41 @@ import client from '../client.js';
 export const createEmployee = async (name, birthday, salary) => {
   const sql = `
     INSERT INTO employees (name, birthday, salary)
-    VALUES ($1, $2, $3)
+    VALUES ($1, $2, $3, $4)
     RETURNING *;
   `;
 
   const {rows: [ createEmployee ] } = await client.query(sql, [name, birthday, salary]);
   return createEmployee;
-}
+};
 
 // === Part 2 ===
 
 /** @returns all employees */
 export const getEmployees = async () => {
   const sql = `
-    SELECT * from employees;
+    SELECT * FROM employees;
     
   `;
 
   const { rows: listOfEmployees } = await client.query(sql);
   return listOfEmployees;
-}
+};
 
 /**
  * @returns the employee with the given id
  * @returns undefined if employee with the given id does not exist
  */
-export async function getEmployee(id) {
-  // TODO
-}
+export const getEmployee = async (id) => {
+  const sql = `
+    SELECT * FROM employees
+    WHERE id = $1;
+  `;
+
+  const { rows: employeeById } = await client.query(sql, [id]);
+  return employeeById[0] || undefined;
+};
+
 
 /**
  * @returns the updated employee with the given id
